172a5819573a29ca9a26e809295c2c5a67abb924
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-18 12:50:14 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,13 +31,18 @@ class PostsController < ApplicationController
     render plain: post.description
   end
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
+
 private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
+   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+
   def post_params
     params.require(:post).permit(:title, :description)
   end
