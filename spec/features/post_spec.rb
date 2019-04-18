f0619139f1d3a29fb82b375c79e6d841756e117b
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-18 12:56:57 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @author1 = Author.create(name: "Stephen King")
-    @post = Post.create(title: "My Post", description: "My post desc", author: @author1)
+    @post = Post.create(title: "My Post", description: "My post desc", name: "Steven")
   end
 
   it 'shows the title on the show page in a h1 tag' do
@@ -26,17 +25,16 @@ describe 'form' do
   it 'shows a new form that submits content and redirects and prints out params' do
     visit new_post_path
 
-    fill_in 'post[title]', with: "My post title"
-    fill_in 'post[description]', with: "My post description"
+    fill_in 'title', with: "My post title"
+    fill_in 'description', with: "My post description"
 
-    click_on "Create Post"
+    click_on "Submit Post"
 
     expect(page).to have_content("My post title")
   end
 
   it 'shows a new form that submits content and redirects and prints out params' do
-    @author1 = Author.create(name: "Stephen King")
-    @post = Post.create(title: "My Post", description: "My post desc", author: @author1)
+    @post = Post.create(title: "My Post", description: "My post desc")
 
     visit edit_post_path(@post)
 
